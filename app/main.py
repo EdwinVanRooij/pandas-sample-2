44ad86388e9666119df51b12791340613cc041ee
Edwin van Rooij <edwinvrooij@gmail.com>
2018-08-14 17:14:03 +0200

    update readme

--- app/main.py
+++ app/main.py
@@ -8,52 +8,66 @@ import json
 
 def main(event, context):
     """Main application flow."""
-    # Show all params for debugging
-    print("MyEvent: ", event)
-    print("MyContext: ", context)
-
-    # Save titles to show later on
-    strings_title = event.get("seriesStringsTitle")
-    numbers_title = event.get("seriesNumbersTitle")
-
-    # Turn lists into Series for DataFrames later on
-    string_series = pd.Series(event.get("seriesStrings"))
-    number_series = pd.Series(event.get("seriesNumbers"))
-
-    # Create DataFrames to enable data manipulation
-    strings_dataframe = pd.DataFrame([string_series])
-    numbers_dataframe = pd.DataFrame([number_series])
-
-    # Manipulate data
-    # ...
-
-    # Visualize strings data
-    print("\n\n" + strings_title)
-    print(strings_dataframe.head())
-
-    # Visualize numbers data
-    print("\n\n" + numbers_title)
-    print(numbers_dataframe.head())
-
-    # Return the DataFrames in JSON format
-    json_string = json.dumps({
-        strings_title: strings_dataframe.to_json(),
-        numbers_title: numbers_dataframe.to_json()
-    })
+    # event = json.loads(event.get('input').get('body'))
+    #
+    # # Show all params for debugging
+    # print("MyEvent: ", event)
+    # print("MyContext: ", context)
+    #
+    # # Save titles to show later on
+    # strings_title = event.get("seriesStringsTitle")
+    # numbers_title = event.get("seriesNumbersTitle")
+    #
+    # # Turn lists into Series for DataFrames later on
+    # string_series = pd.Series(event.get("seriesStrings"))
+    # number_series = pd.Series(event.get("seriesNumbers"))
+    #
+    # # Create DataFrames to enable data manipulation
+    # strings_dataframe = pd.DataFrame([string_series])
+    # numbers_dataframe = pd.DataFrame([number_series])
+    #
+    # # Manipulate data
+    # # ...
+    #
+    # # Visualize strings data
+    # print("\n\n" + strings_title)
+    # print(strings_dataframe.head())
+    #
+    # # Visualize numbers data
+    # print("\n\n" + numbers_title)
+    # print(numbers_dataframe.head())
+    #
+    # # Return the DataFrames in JSON format
+    # string_json = json.dumps({
+    #     strings_title: strings_dataframe.to_json(),
+    #     numbers_title: numbers_dataframe.to_json()
+    # })
 
     # Must be in this format for Amazon API gateway
     return {
         'statusCode': 200,
-        'headers': {'Content-Type': 'application/json'},
-        'body': json_string
+        'body': json.dumps({'input': 'something more',
+                            'busy': "something",
+                            'guest_email': "something"})
     }
 
 
 if __name__ == "__main__":
+    # event = {
+    #     "seriesStringsTitle": "A Series Of Strings",
+    #     "seriesNumbersTitle": "A Series Of Numbers",
+    #     "seriesStrings": ["value1", "value2", "value3", "value4"],
+    #     "seriesNumbers": [1, 2, 3, 4, 5]
+    # }
     event = {
-        "seriesStringsTitle": "A Series Of Strings",
-        "seriesNumbersTitle": "A Series Of Numbers",
-        "seriesStrings": ["value1", "value2", "value3", "value4"],
-        "seriesNumbers": [1, 2, 3, 4, 5]
+        "input": {
+            "resource": "/myPandasFunction",
+            "path": "/myPandasFunction",
+            "httpMethod": "POST",
+            "body": "{\n        \"seriesStringsTitle\": \"A Series Of Strings\",\n        \"seriesNumbersTitle\": \"A Series Of Numbers\",\n        \"seriesStrings\": [\"value1\", \"value2\", \"value3\", \"value4\"],\n        \"seriesNumbers\": [1, 2, 3, 4, 5]\n    }",
+            "isBase64Encoded": False
+        },
+        "busy": "something",
+        "guest_email": "something"
     }
     print(main(event, None))
--- readme.md
+++ readme.md
@@ -39,3 +39,34 @@ Add libraries (run from `lib/python3.6/site-packages`\*\*)
 __Below is deprecated.__
 _This is the final command to create a zip in one line, when running Linux with the Fish shell interpreter, from `app/`._
 `zip -g PandasSample.zip main.py; and zip -r9 PandasSample.zip ../lib/python3.6/site-packages/`
+
+
+## Notes
+
+There are 4 different testing scenario's to accomplish the use case @ Peter's mail:
+
+- 1. Testing the lambda locally using a locally-defined test event (in the python script).
+This is the first stage. Everything should work here before moving on.
+We're verifying if the base functionality of the script works here.
+
+- 2. Testing the lambda directly from the web interface using a there-defined test event.
+This is the second step. Upload the local environment to aws lambda, check if it works there.
+It's required to replicate the test event locally into a json test event on the web interface here.
+
+
+- 3. Testing the lambda via the web interface using an API gateway.
+This adds another conversion layer to the test event. This means that the initial test event is not as you define it. When you add a way to connect to the lambda using the HTTP protocol, so by using the AWS API gateway, this gateway transforms the event you sent in the http post body. It adds a lot of meta data to it, like this:
+```
+            "resource": "/myPandasFunction",
+            "path": "/myPandasFunction",
+            "httpMethod": "POST",
+            "body": "{\n        \"seriesStringsTitle\": \"A Series Of Strings\",\n        \"seriesNumbersTitle\": \"A Series Of Numbers\",\n        \"seriesStrings\": [\"value1\", \"value2\", \"value3\", \"value4\"],\n        \"seriesNumbers\": [1, 2, 3, 4, 5]\n    }",
+            "isBase64Encoded": False
+    }
+```
+Where `body` in `input` is the only thing you've passed it (through the http post request body).
+It's necessary to change the python script after adding an api gateway, because the `event` param now contains all of the extra meta data, instead of just the previous `event`.
+(It's also necessary to change the test event when directly testing the lambda using the lambda test web interface, because the script will now expect the meta data.)
+
+- 4. Testing the lambda via CURL using an API gateway.
+For some reason there's a difference between testing the api gateway through curl (or any other http client) and the amazon api gateway testing interface.
